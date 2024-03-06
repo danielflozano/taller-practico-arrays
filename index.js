5acbc914bce93a40b2435db81d725a2fc9f3a72a
Daniel Lozano <danyfriko@gmail.com>
2024-03-05 23:07:52 -0500

    Implementacion constructor array

--- index.js
+++ index.js
@@ -11,3 +11,7 @@ console.log(frutas);
 // 3. Crear un array con elementos de diferentes tipos de datos:
 let diferentesDatos = [true, 25, 'Hola']
 console.log(diferentesDatos);
+
+// 4. Crear un array utilizando el constructor Array():
+const arr = new Array( 3 );
+console.log(arr);
