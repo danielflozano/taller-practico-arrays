e0ddf0bf2d4cd93f0dff5c9c1e0b2c0d934610a4
Daniel Lozano <danyfriko@gmail.com>
2024-03-05 22:56:11 -0500

    Implementacion array con elementos

--- index.js
+++ index.js
@@ -3,3 +3,7 @@
 // 1. Crear un array vacío:
 let arrayVacio = [];
 console.log(arrayVacio);
+
+// 2. Crear un array con elementos:
+let frutas = ['Manzana', 'Pera', 'Mango', 'Banano'];
+console.log(frutas);
