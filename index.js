451dddddca7b773a620c5b80c3dc78fa68d2302b
Daniel Lozano <danyfriko@gmail.com>
2024-03-05 23:02:09 -0500

    Implementacion array con diferentes tipos de datos

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ console.log(arrayVacio);
 // 2. Crear un array con elementos:
 let frutas = ['Manzana', 'Pera', 'Mango', 'Banano'];
 console.log(frutas);
+
+// 3. Crear un array con elementos de diferentes tipos de datos:
+let diferentesDatos = [true, 25, 'Hola']
+console.log(diferentesDatos);
